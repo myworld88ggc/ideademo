4a97438842a9058c86468f9ba0c9fa10e1e004d5
MyWorld88ggc <myworld88ggc@gmail.com>
2017-09-12 12:30:39 +0800

    init

---
+++ springwardemo/src/main/java/com/will/SpringwardemoApplication.java
@@ -0,0 +1,12 @@
+package com.will;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwardemoApplication.class, args);
+	}
+}
